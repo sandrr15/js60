6a1d521cda1612bbecc67bacb3ad75fe71e9adbf
sandrr15 <joaovitorenzopsn@gmail.com>
2026-02-12 21:37:57 -0300

    Add files via upload

--- CALCULADORA JS/script.js
+++ CALCULADORA JS/script.js
@@ -3,7 +3,8 @@ function somar(){
 const numero1 = parseFloat(document.getElementById('numero1').value);
 const numero2 = parseFloat(document.getElementById('numero2').value);
 const soma = (numero1 + numero2);
-document.getElementById('resultado').innerHTML = 'SOMA:' + soma
+return document.getElementById('resultado').innerHTML =  + soma
+
 
 }
 
@@ -11,5 +12,25 @@ function subtrair(){
 
 const numero1 = parseFloat(document.getElementById('numero1').value);
 const numero2 = parseFloat(document.getElementById('numero2').value);
+const subtrair = (numero1 - numero2);
+return document.getElementById('resultado').innerHTML =  + subtrair
+
+}
+
+function dividir(){
+
+const numero1 = parseFloat(document.getElementById('numero1').value);
+const numero2 = parseFloat(document.getElementById('numero2').value);
+const dividir = (numero1 / numero2);
+return document.getElementById('resultado').innerHTML =  + dividir
+
+}
+
+function multiplicar(){
+
+const numero1 = parseFloat(document.getElementById('numero1').value);
+const numero2 = parseFloat(document.getElementById('numero2').value);
+const multi = (numero1 * numero2);
+return document.getElementById('resultado').innerHTML =  + multi
 
 }
