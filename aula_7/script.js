8e6de42a78a2a8107581f807d8f5d407e8879155
sandrr15 <joaovitorenzopsn@gmail.com>
2026-02-19 21:44:50 -0300

    Add files via upload

---
+++ aula_7/script.js
@@ -0,0 +1,91 @@
+//verificaçao de idade
+
+//let idade = prompt("digite sua idade")
+
+//if(idade < 18){
+
+//alert('voce é de menor')
+
+//}
+//else if(idade > 18){
+
+//alert('voce e de maior')
+
+//}
+
+//par ou impar
+
+//let par_im = prompt('digite um numero')
+
+//if(par_im % 2 ==0){
+
+//alert('numero par')
+
+//}
+//else{
+
+//alert('numero impar')
+
+//}
+
+//clasificador de triangulos
+
+//let lado1 = prompt('digite um numero')
+//let lado2 = prompt('digite um numero')
+//let lado3 = prompt('digite um numero')
+
+//if(lado1 == lado2 && lado2 == lado3){
+
+//alert('todos os lados sao iguais')
+
+//}else if(lado1 === lado2 || lado1 === lado3 || lado2 === lado3){
+
+//alert('dois lados iguais')
+
+//}else{
+
+//alert('todos os lados sao diferentes')
+
+//}
+
+//multiplo de 8 ou 16
+
+//let numero1 = promp('digite um numero')
+
+//if(numero1 % 8 ==0 && numero1 % 16 ==0){
+
+//alert('esse numero e multiplo de 8 e 16 simultaneamente')
+
+//}else{
+
+//alert('esse numero nao e multiplo de 8 ou 16 simultaneamente')
+
+//}
+
+//numero positivo e maior que 15
+
+//let num1 = prompt('digite um numero')
+
+//if(num1 > 0 && num1 < 15){
+
+//alert('esse numero é positivo e maior que 15')
+
+//}else{
+
+//alert('esse numero é negativo e menor que 15')
+
+//}
+
+//divisivel por 9 ou por 6
+
+//let num1 = prompt('digite um numero')
+
+//if(num1 / 9==0 || num1/ 6==0){
+
+//alert('esse numero e divisivel por 9 ou por 6')
+
+//}else{
+
+//alert('esse numero nao e divisivel por 9 ou por 6')
+
+//}
---
+++ reserva_de_restaurantes/script.js
@@ -0,0 +1,26 @@
+let client1 = prompt('qual seu nome:')
+let client_num1= prompt('quantas pessoas seriam?').value
+let mesa = alert('1:mesa comum 2:mesa espaçosa 3:mesa de luxo')
+let mesa_client1 = prompt('escolha sua mesa:').value
+
+let preco_mesa_comum = 50.
+let preco_mesa_espacosa= 80
+let preco_mesa_de_luxo=100
+
+
+if (mesa_client1 === 1){
+
+alert('voce escolheu a mesa comum'(preco_mesa_comum))
+
+}else if(mesa_client1 === 2){
+
+alert('voce escolheu a mesa espaçosa'(preco_mesa_espacosa))
+
+}else if(mesa_client1 === 3){
+
+alert('voce escolheu a mesa de luxo'(preco_mesa_de_luxo))
+
+}
+
+let valor_total = mesa_client1 + 50 
+alert(valor_total)
